016308ef1875fa69a00b57412cdd8204e416c229
Breesho <felix.noel3@orange.fr>
2020-04-30 14:09:35 +0200

    push

--- Exercice-6/assets/js/script.js
+++ Exercice-6/assets/js/script.js
@@ -1,19 +1,19 @@
 
 
 let idList = document.getElementById("list");
-let firstNumber = 2
-let secondNumber = 4
-let n = 0;
+let firstNumber = 10;
+let secondNumber = 2;
+let n = 10 ;
 let idButton = document.getElementById("mybutton");
 
 idButton.addEventListener("click", function() {
 
-  while (n < 250) {
-    n = firstNumber * secondNumber;
+  while (n > 1) {
+    n = firstNumber / secondNumber;
     let myP = document.createElement("p");
-    myP.innerHTML = secondNumber;
+    myP.innerHTML = firstNumber;
     idList.appendChild(myP);
-    secondNumber = n;
+    firstNumber= n;
 }
 
 })
